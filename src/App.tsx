7ce41f823bc7f7835754cde81a92ed1a789ce430
mamdouhamr-1111 <mamdouhamr@gmail.com>
2026-05-28 17:56:08 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{padding:"40px"}}>
+      <h1>Honeymoons & Co.</h1>
+      <p>Your website is connected successfully.</p>
+    </div>
+  );
+}
