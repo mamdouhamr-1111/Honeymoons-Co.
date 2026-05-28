66fb86337730b688362b898a74a8fdb07c46fadd
mamdouhamr-1111 <mamdouhamr@gmail.com>
2026-05-28 18:05:45 +0300

    Delete App.tsx

--- App.tsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{padding:"40px"}}>
-      <h1>Honeymoons & Co.</h1>
-      <p>Your website is connected successfully.</p>
-    </div>
-  );
-}
